c71e2edcaf9aa8bcb17dbba2126303842c900b67
Alberto Teixeira <albertothe@gmail.com>
2019-10-02 20:47:09 -0300

    continuação vendas

--- teste.php
+++ teste.php
@@ -1,25 +1,24 @@
-<?php
-require_once ("includes/conexao.php");
-
-try {
-	
-	
-	$query = $pdo->prepare("SELECT atualizaestoque('E',6,500,'40',1,'V',1);");
-	
-	//$query->bindValue(1, (int)($_POST['idade']));
-	
-	if($query->execute() === false){
-		$error = $query->errorInfo();
-		echo '<font color=\'#ff0000\'>', substr($error[2], 7), '</font>'; 
-	} else {
-		$query = null;
-		$pdo = null;
-		echo '<font color=\'#0000ff\'>Cadastrado com sucesso!</font>';
-		
-	}
-} catch(PDOException $pex){
-	echo '<font color=\'#ff0000\'>Ocorreu um erro com a base de dados.</font>';
-	//loga o erro e etc
-}
-
-?>
+<html>
+<head>
+<script type="text/javascript" src="jquery.js"></script>
+<script>
+document.addEventListener('click', function(e) {
+	  
+	  var self = e.target;
+	  
+	  if(['entrada','saida'].indexOf(self.id) !== -1) {
+	    var el = document.getElementById(self.id === 'entrada' ? 'saida' : 'entrada');
+	    
+	    self.removeAttribute('disabled');
+	    
+	    el.setAttribute('disabled','');
+	    el.value = "";
+	  }
+	})
+</script>
+</head>
+<body>
+<input type="text" id="entrada" name="entrada">     
+<input type="text" id="saida" name="saida">
+</body>
+</html>
